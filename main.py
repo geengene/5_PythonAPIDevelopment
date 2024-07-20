07f77ebb1a8cdf90d43934224ad5078f739cdbea
gene <gqkuah2505@gmail.com>
2024-07-20 08:37:45 +0800

    modified:   __pycache__/main.cpython-312.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # https://aws.amazon.com/what-is/api/#:~:text=API%20stands%20for%20Application%20Programming,other%20using%20requests%20and%20responses.
 from typing import Optional, Union
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -12,6 +13,14 @@ class Post(BaseModel):
   published: bool = True # defaults to true
   rating: Optional[int] = None # optional user input, else default to None. if value is not an int, return error 
 
+my_posts = [{"title": "title 1", "content": "content 1", "id": 1}, 
+            {"title": "title 2", "content": "content 2", "id": 2}] 
+
+def find_post(id):
+  for p in my_posts:
+    if p["id"] == id:
+      return p
+
 @app.get("/")
 def read_root():
   return {"Hello": "Worldiiiii"}
@@ -21,7 +30,6 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
   return {"item_id": item_id, "q": q}
 
-
 # You already created an API that:
 
 # Receives HTTP requests in the paths / and /items/{item_id}.
@@ -30,15 +38,25 @@ def read_item(item_id: int, q: Union[str, None] = None):
 # The path /items/{item_id} has an optional str query parameter q.
 
 
-@app.get("/posts")
+@app.get("/posts") # retreiving preexisting posts from server to user
 def get_posts():
-  return {"data": "this is your data"}
+  return {"data": my_posts} # sends my_posts back to user
 
 
-@app.post("/createposts")
-def create_posts(post: Post):  # Post will validate that there is a title and content data with str value 
-  print(post)
-  print(post.model_dump()) # sends back a dictonary
-  return {"data": "new post"}
-                          
-# schema
+@app.post("/posts", status_code=status.HTTP_201_CREATED) # user to server
+def create_posts(post: Post):  # Post will validate that there is a title and content data with str value, when creating an entity, return a 201 status
+  post_dict = post.model_dump()
+  post_dict['id'] = randrange(0, 100000000)
+  my_posts.append(post_dict) # adds post post_dict in Body(Postman) to array my_posts
+  return {"data": post_dict}
+
+@app.get("/posts/{id}") # path parameter
+def get_post(id: int): # response: Response): # validates that id is an integer. no longer necessary to convert to integer by ourselves.
+  post = find_post(id) # id passed from get_post is a string as the path parameter
+  if not post:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} not found")
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    # return {"message": f"post with id:{id} not found"}
+  return {"post_detail": post}
+
+# @app.get("posts/latest")
