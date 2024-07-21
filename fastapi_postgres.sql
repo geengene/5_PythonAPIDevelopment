b388d662d409fbdee51ac557b76530dc5ea7e063
gene <gqkuah2505@gmail.com>
2024-07-21 11:33:22 +0800

    modified:   fastapi_postgres.sql

--- fastapi_postgres.sql
+++ fastapi_postgres.sql
@@ -1,2 +1,5 @@
-SELECT * FROM products;
-SELECT id as products_id, name, price FROM products
+SELECT id as product_id, name, price , inventory FROM products ORDER BY price DESC;
+SELECT * FROM products WHERE name LIKE 'TV%';
+SELECT * FROM products ORDER BY id LIMIT 5 OFFSET 5;
+-- INSERT INTO products (name, price, inventory) VALUES ('tortilla', 4, 1000), ('car', 20000, 5), ('toast', 1, 40) returning *; 
+-- DELETE FROM products WHERE id = 1 RETURNING *;
